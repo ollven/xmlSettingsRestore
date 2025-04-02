d1ab695f2e68085758602674b963ac51ad05fa71
ollven <ollven@TeamCity>
2025-04-02 14:39:04 +0200

    Versioned settings configuration updated (TeamCity change in 'XML_Settings_REstore' project)

---
+++ .teamcity/settings.kts
@@ -0,0 +1,75 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2024.12"
+
+project {
+
+    buildType(BuildWithTemplate)
+    buildType(BuildToTemplate)
+
+    template(id3Steps)
+}
+
+object BuildToTemplate : BuildType({
+    templates(id3Steps)
+    name = "Build to template"
+})
+
+object BuildWithTemplate : BuildType({
+    templates(id3Steps)
+    name = "BuildWithTemplate"
+
+    steps {
+        script {
+            name = "OneMore Step"
+            id = "OneMore_Step"
+            scriptContent = "ps -a"
+        }
+    }
+})
+
+object id3Steps : Template({
+    id("3Steps")
+    name = "3Steps"
+
+    steps {
+        script {
+            name = "Step1"
+            id = "Step1"
+            scriptContent = "pwd"
+        }
+        script {
+            name = "Step2"
+            id = "Step2"
+            scriptContent = "ls"
+        }
+        script {
+            name = "Step3"
+            id = "Step3"
+            scriptContent = "ls -a"
+        }
+    }
+})
